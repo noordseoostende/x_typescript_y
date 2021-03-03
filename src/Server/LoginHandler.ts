f91401d27e388e127364d0f1ec212a0c7eebb9c7
noordseoostende <voropayev1@mail.ru>
2021-03-03 16:26:12 +0100

    536

--- src/Server/LoginHandler.ts
+++ src/Server/LoginHandler.ts
@@ -1,4 +1,5 @@
 import { IncomingMessage, ServerResponse } from "http";
+import { HTTP_CODES, HTTP_METHODS } from "../Shared/Model";
 import { Account, Handler, TokenGenerator } from "./Model";
 
 export class LoginHandler implements Handler {
@@ -9,24 +10,49 @@ export class LoginHandler implements Handler {
   public constructor(req: IncomingMessage, res: ServerResponse, tokenGenerator: TokenGenerator){
     this.req = req;
     this.res = res;
-    this.tokenGenerator = tokenGenerator;
+    this.tokenGenerator = tokenGenerator
   }
 
   public async handleRequest(): Promise<void> {
+    switch (this.req.method) {
+      case HTTP_METHODS.POST:
+        await this.handlePost();
+        break;
+    
+      default:
+        this.handleNotFound();
+        break;
+    }
+  }
+
+  private async handleNotFound(){
+    this.res.statusCode = HTTP_CODES.NOT_FOUND;
+    this.res.write('not found');
+  }
+
+
+    
+  
+
+  private async handlePost(){
     try {
       
       const body = await this.getRequestBody();
       const sessionToken = await this.tokenGenerator.generateToken(body);
       if (sessionToken) {
-        this.res.write('geldige referenties');
+        this.res.statusCode = HTTP_CODES.CREATED,
+        this.res.writeHead(HTTP_CODES.CREATED, {'Content-Type': 'application'});
+        this.res.write(JSON.stringify(sessionToken));
       } else {
-        this.res.write('verkeerde referenties');
+        this.res.statusCode = HTTP_CODES.NOT_FOUND;
+        this.res.write('wrong username or password');
   
       }
     } catch (error) {
-      this.res.write('error: ' + error.message)
+      this.res.write('error: ' + error.message);
     }
   }
+
   private getRequestBody(): Promise<Account> {
     return new Promise((resolve, reject) => {
       let body = '';
